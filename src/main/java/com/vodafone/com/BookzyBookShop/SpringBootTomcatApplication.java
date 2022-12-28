b2be6b845dd0739f74d3ac481fa817fb286e852d
Adrian <38786314+elmonje@users.noreply.github.com>
2022-12-28 21:03:44 +0100

    Delete SpringBootTomcatApplication.java

--- src/main/java/com/vodafone/com/BookzyBookShop/SpringBootTomcatApplication.java
+++
@@ -1,9 +0,0 @@
-package com.vodafone.com.BookzyBookShop;
-
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class SpringBootTomcatApplication extends SpringBootServletInitializer {
-
-}
