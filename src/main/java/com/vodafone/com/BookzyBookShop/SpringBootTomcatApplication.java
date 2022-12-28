3bf840eeefe873a81cf6a3407db450655e9d31e9
Adrian <38786314+elmonje@users.noreply.github.com>
2022-12-28 19:38:57 +0100

    Update SpringBootTomcatApplication.java

--- src/main/java/com/vodafone/com/BookzyBookShop/SpringBootTomcatApplication.java
+++ src/main/java/com/vodafone/com/BookzyBookShop/SpringBootTomcatApplication.java
@@ -1,5 +1,9 @@
 package com.vodafone.com.BookzyBookShop;
 
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class SpringBootTomcatApplication extends SpringBootServletInitializer {
+
 }
