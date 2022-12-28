c818f32bd93e598379062dc57f053deccd45aa4e
Adrian <38786314+elmonje@users.noreply.github.com>
2022-12-28 20:52:17 +0100

    Update BookzyBookShopApplication.java

--- src/main/java/com/vodafone/com/BookzyBookShop/BookzyBookShopApplication.java
+++ src/main/java/com/vodafone/com/BookzyBookShop/BookzyBookShopApplication.java
@@ -2,9 +2,10 @@ package com.vodafone.com.BookzyBookShop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BookzyBookShopApplication {
+public class BookzyBookShopApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookzyBookShopApplication.class, args);
